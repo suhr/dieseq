850a51f0a7edb43bed66d951aec1f0cf37a01e59
Сухарик <suhr@i2pmail.org>
2018-01-29 17:01:58 +0300

    Write some code

---
+++ src/main.rs
@@ -0,0 +1,387 @@
+#![allow(dead_code)]
+
+#[macro_use] extern crate gfx;
+extern crate gfx_device_gl;
+extern crate gfx_window_glutin;
+extern crate glutin;
+extern crate cgmath;
+extern crate palette;
+
+extern crate portmidi;
+
+use gfx::Device;
+use gfx_window_glutin::init as gfx_init;
+use cgmath::{Vector2, ElementWise};
+
+use renderer::{ColorFormat, DepthFormat, Draw};
+
+mod renderer;
+mod ui;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Note {
+    pub channel: u16,
+    pub time: (i16, i16),
+    pub pitch: i16,
+}
+
+#[derive(Debug, Clone)]
+struct Score {
+    measure_ticks: u16,
+    notes: Vec<Note>,
+}
+
+impl Score {
+    pub fn new() -> Self {
+        Score {
+            measure_ticks: 16,
+            notes: vec![],
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Msg {
+    WindowEvent(glutin::WindowEvent),
+    MouseWheel {
+        position: Vector2<f32>,
+        delta: (f32, f32),
+    },
+    LeftPressed {
+        position: Vector2<f32>,
+    },
+    LeftReleased {
+        position: Vector2<f32>,
+    },
+    LeftDrag {
+        vector: Vector2<f32>,
+    },
+    RightDrag {
+        vector: Vector2<f32>,
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Intent {
+    mailbox: Vec<Msg>,
+    screen_size: Vector2<f32>,
+    mouse_pos: Vector2<f32>,
+    lbutton_pressed: Option<std::time::Instant>,
+    rbutton_pressed: Option<std::time::Instant>,
+}
+
+impl Intent {
+    fn new() -> Self {
+        Intent {
+            mailbox: vec![],
+            screen_size: [1024.0, 768.0].into(),
+            mouse_pos: Vector2::new(0.0, 0.0),
+            lbutton_pressed: None,
+            rbutton_pressed: None,
+        }
+    }
+
+    fn intent(&mut self, event: glutin::WindowEvent) {
+        use glutin::WindowEvent::*;
+        match event {
+            MouseWheel {delta, ..} => {
+                if let glutin::MouseScrollDelta::LineDelta(x, y) = delta {
+                    self.mailbox.push(Msg::MouseWheel {
+                        position: self.mouse_pos,
+                        delta: (x, y),
+                    })
+                }
+            },
+            MouseInput { device_id, modifiers, button, state, ..} => {
+                use glutin::{MouseButton as Mb, ElementState as Es};
+                match (button, state) {
+                    (Mb::Right, Es::Pressed) => {
+                        self.rbutton_pressed = Some(std::time::Instant::now())
+                    },
+                    (Mb::Right, Es::Released) => {
+                        self.rbutton_pressed = None
+                    },
+                    (Mb::Left, Es::Pressed) => {
+                        self.lbutton_pressed = Some(std::time::Instant::now());
+                        self.mailbox.push(Msg::LeftPressed {
+                            position: self.mouse_pos,
+                        })
+                    },
+                    (Mb::Left, Es::Released) => {
+                        self.lbutton_pressed = None;
+                        self.mailbox.push(Msg::LeftReleased {
+                            position: self.mouse_pos,
+                        })
+                    },
+                    _ => {},
+                }
+
+                self.mailbox.push(Msg::WindowEvent(
+                    MouseInput { device_id, modifiers, button, state }
+                ));
+
+            },
+            CursorMoved { position, ..} => {
+                let position = Vector2::new(position.0 as f32, self.screen_size.y - position.1 as f32);
+
+                if let Some(instant) = self.lbutton_pressed {
+                    if instant.elapsed() >= std::time::Duration::from_millis(50) {
+                        self.mailbox.push(Msg::LeftDrag {
+                            vector: position - self.mouse_pos
+                        });
+                    }
+                }
+                if let Some(instant) = self.rbutton_pressed {
+                    if instant.elapsed() >= std::time::Duration::from_millis(50) {
+                        self.mailbox.push(Msg::RightDrag {
+                            vector: position - self.mouse_pos
+                        });
+                    }
+                }
+                self.mouse_pos = position;
+            },
+            Resized(x, y) => {
+                self.screen_size = [x as f32, y as f32].into();
+                self.mailbox.push(Msg::WindowEvent(Resized(x, y)))
+            },
+            ev =>
+                self.mailbox.push(Msg::WindowEvent(ev))
+        }
+    }
+
+    fn messages(&mut self) -> Vec<Msg> {
+        let mut mb = vec![];
+        ::std::mem::swap(&mut mb, &mut self.mailbox);
+        mb
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Brick {
+    time: (f32, f32),
+    pitch: f32,
+}
+
+impl From<Brick> for Note {
+    fn from(brick: Brick) -> Self {
+        let pitch = brick.pitch.round() as i16;
+
+        let (t0, t1) = brick.time;
+        let time =
+            if t0 <= t1 {
+                (t0.round() as i16, t1.round() as i16)
+            } else {
+                (t1.round() as i16, t0.round() as i16)
+            };
+
+        Note {
+            channel: 0,
+            time, pitch
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum State {
+    Idle,
+    Drawing(Brick),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Tool {
+    Arrow,
+    Pencil,
+}
+
+#[derive(Debug, Clone)]
+struct Model {
+    state: State,
+    tool: Tool,
+    grid: ui::Grid,
+    score: Score,
+}
+
+impl Model {
+    fn new() -> Self {
+        let mut score = Score::new();
+
+        let grid = ui::Grid::new(
+            Vector2::new(1024.0, 768.0),
+            (Vector2::new(0.0, 0.0), Vector2::new(12.0, 155.0))
+        );
+
+        Model {
+            state: State::Idle,
+            tool: Tool::Pencil,
+            grid,
+            score: score,
+        }
+    }
+}
+
+fn model(mut model: Model, msg: Msg) -> Model {
+    use glutin::WindowEvent::*;
+    use Msg::*;
+
+    match msg {
+        Msg::MouseWheel { position, delta: (_, y) } => {
+            let start = model.grid.view.0.x;
+            let end = model.grid.view.1.x;
+            let y = -y;
+
+            let k =
+                if y < 0.0 || (end - start) <= model.grid.size.x / 16.0 {
+                    (1.07_f32).powf(y)
+                }
+                else {
+                    1.0
+                };
+
+            let split_ratio = position.x / model.grid.size.x;
+            let split_point = start + (end - start) * split_ratio;
+
+            model.grid.view.0.x = (start - split_point) * k + split_point;
+            model.grid.view.1.x = (end - split_point) * k + split_point;
+
+        },
+        Msg::LeftPressed { position } if model.tool == Tool::Pencil => {
+            let view_pos = model.grid.view.0 + model.grid.scale_vector(position);
+
+            let time = view_pos.x * model.score.measure_ticks as f32;
+            let pitch = view_pos.y;
+
+            model.state = State::Drawing(Brick {
+                time: (time, time),
+                pitch,
+            });
+        },
+        Msg::LeftReleased { .. } => {
+            if let State::Drawing(brick) = model.state {
+                if brick.time.0.round() != brick.time.1.round() {
+                    model.score.notes.push(brick.into())
+                }
+                else if brick.time.0 == brick.time.1 {
+                    let (time, pitch) = (brick.time.0, brick.pitch);
+
+                    model.score.notes.retain(|n|
+                        (n.pitch as f32 - pitch).abs() >= 0.5
+                        || {
+                            (n.time.0 as f32) > time
+                            || (n.time.1 as f32) < time
+                        }
+                    )
+                }
+
+                model.state = State::Idle
+            }
+        },
+        Msg::LeftDrag { vector } => {
+            let shift = model.grid.scale_vector(vector);
+
+            if let State::Drawing(brick) = model.state {
+                let brick = Brick {
+                    time: (brick.time.0, brick.time.1 + shift.x * model.score.measure_ticks as f32),
+                    pitch: brick.pitch + shift.y,
+                };
+
+                model.state = State::Drawing(brick)
+            }
+        },
+        Msg::RightDrag { vector } => {
+            let shift = -model.grid.scale_vector(vector);
+
+            let v0 = model.grid.view.0 + shift;
+            let v1 = model.grid.view.1 + shift;
+
+            if
+                (shift.y > 0.0 && v1.y <= 31.0 * 8.0)
+                || (shift.y < 0.0 && v0.y >= 0.0)
+            {
+                model.grid.view.0.y = v0.y;
+                model.grid.view.1.y = v1.y;
+            }
+
+            model.grid.view.0.x = v0.x;
+            model.grid.view.1.x = v1.x;
+        },
+        WindowEvent(Resized(x, y)) =>
+            model.grid.size = Vector2::new(x as f32, y as f32),
+        _ => (),
+    }
+
+    model
+}
+
+fn draw(model: &Model, screen_size: [f32; 2], renderer: &mut renderer::Renderer) {
+    use renderer::Render;
+    renderer.clear(model.grid.style.base3());
+    model.grid.draw(screen_size.into(), renderer);
+
+    let mut notes = model.score.notes.clone();
+
+    if let State::Drawing(brick) = model.state {
+        notes.push(brick.into())
+    }
+
+    ui::NoteView {
+        notes,
+        measure_ticks: model.score.measure_ticks,
+        style: model.grid.style,
+        view: model.grid.view,
+    }
+    .draw(screen_size.into(), renderer);
+}
+
+pub fn main() {
+    use glutin::GlContext;
+
+    let mut events_loop = glutin::EventsLoop::new();
+
+    let builder = glutin::WindowBuilder::new()
+        .with_title("Dieseq".to_string())
+        .with_dimensions(1024, 768);
+    let context = glutin::ContextBuilder::new()
+        .with_multisampling(8)
+        .with_vsync(true);
+    let (window, mut device, mut factory, main_color, mut main_depth) =
+        gfx_init::<ColorFormat, DepthFormat>(builder, context, &events_loop);
+
+    let encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+    let mut renderer = renderer::Renderer::new(factory, encoder, main_color);
+
+    let mut intent = Intent::new();
+    let mut the_model = Model::new();
+
+    let mut running = true;
+    let mut screen_size = [1024.0, 768.0];
+    while running {
+        events_loop.poll_events(|ev| {
+            use glutin::WindowEvent::*;
+            if let glutin::Event::WindowEvent {event, ..} = ev {
+                match event {
+                    Closed =>
+                        running = false,
+                    Resized(w, h) => {
+                        screen_size = [w as f32, h as f32];
+                        intent.intent(Resized(w, h));
+                        renderer.update_views(&window, &mut main_depth);
+                    },
+                    ev =>
+                        intent.intent(ev),
+                }
+            }
+        });
+
+        for m in intent.messages() {
+            the_model = model(the_model, m);
+        }
+        draw(&the_model, screen_size, &mut renderer);
+
+        renderer.draw(screen_size, &mut device);
+        window.swap_buffers().unwrap();
+        device.cleanup();
+
+        // let dt = ::std::time::Duration::from_millis(10);
+        // ::std::thread::sleep(dt)
+    }
+}
---
+++ src/ui.rs
@@ -0,0 +1,199 @@
+use ::palette::pixel::Srgb;
+use ::cgmath::{ElementWise, Vector2};
+
+use ::renderer::{Draw, Render};
+
+#[derive(Debug, Clone, Copy)]
+// Hard-coded Solarized theme
+// This is wrong, but I'm lazy. Goodbye, corners! ✂
+pub enum Style {
+    Light,
+    Dark,
+}
+
+fn u8_to_rgb(r: u8, g: u8, b: u8) -> [f32; 4] {
+    let rgb: ::palette::Rgb = Srgb::new_u8(r, g, b).into();
+    rgb.to_pixel()
+}
+
+impl Style {
+    pub fn inverse(&self) -> Self {
+        match *self {
+            Style::Light => Style::Dark,
+            Style::Dark => Style::Light,
+        }
+    }
+
+    pub fn base0(&self) -> [f32; 4] {
+        match *self {
+            Style::Light => u8_to_rgb(0x83, 0x94, 0x96),
+            Style::Dark => u8_to_rgb(0x65, 0x7b, 0x83),
+        }
+    }
+
+    pub fn base1(&self) -> [f32; 4] {
+        match *self {
+            Style::Light => u8_to_rgb(0x93, 0xa1, 0xa1),
+            Style::Dark => u8_to_rgb(0x58, 0x6e, 0x75),
+        }
+    }
+
+    pub fn base2(&self) -> [f32; 4] {
+        match *self {
+            Style::Light => u8_to_rgb(0xee, 0xe8, 0xd5),
+            Style::Dark => u8_to_rgb(0x07, 0x36, 0x42),
+        }
+    }
+
+    pub fn base3(&self) -> [f32; 4] {
+        match *self {
+            Style::Light => u8_to_rgb(0xfd, 0xf6, 0xe3),
+            Style::Dark => u8_to_rgb(0x00, 0x2b, 0x36),
+        }
+    }
+
+    pub fn yellow(&self) -> [f32; 4] {
+        u8_to_rgb(0xb5, 0x89, 0x00)
+    }
+
+    pub fn orange(&self) -> [f32; 4] {
+        u8_to_rgb(0xcb, 0x4b, 0x16)
+    }
+
+    pub fn red(&self) -> [f32; 4] {
+        u8_to_rgb(0xdc, 0x32, 0x2f)
+    }
+
+    pub fn magenta(&self) -> [f32; 4] {
+        u8_to_rgb(0xd3, 0x36, 0x82)
+    }
+
+    pub fn violet(&self) -> [f32; 4] {
+        u8_to_rgb(0x6c, 0x71, 0xc4)
+    }
+
+    pub fn blue(&self) -> [f32; 4] {
+        u8_to_rgb(0x26, 0x8b, 0xd2)
+    }
+
+    pub fn cyan(&self) -> [f32; 4] {
+        u8_to_rgb(0x2a, 0xa1, 0x98)
+    }
+
+    pub fn green(&self) -> [f32; 4] {
+        u8_to_rgb(0x85, 0x99, 0x00)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Grid {
+    pub size: Vector2<f32>,
+    pub view: (Vector2<f32>, Vector2<f32>),
+    pub beats: u8,
+    pub style: Style,
+    thin_width: f32,
+    thick_width: f32,
+}
+
+impl Grid {
+    pub fn new(size: Vector2<f32>, view: (Vector2<f32>, Vector2<f32>)) -> Self {
+        Grid {
+            size, view,
+            style: Style::Dark,
+            beats: 4,
+            thin_width: 1.0,
+            thick_width: 2.0,
+        }
+    }
+
+    pub fn scale_vector(&self, position: Vector2<f32>) -> Vector2<f32> {
+        let view_size = self.view.1 - self.view.0;
+
+        position.div_element_wise(self.size).mul_element_wise(view_size)
+    }
+}
+
+impl Draw for Grid {
+    fn draw<R: Render>(&self, size: Vector2<f32>, renderer: &mut R) {
+        let (v0, v1) = self.view;
+        let v_size = v1 - v0;
+        let aspect = size.div_element_wise(v_size);
+
+        let (y_first, y_last) = (
+            v0.y.ceil() as i32,
+            v1.y.floor() as i32
+        );
+        for line in y_first..(y_last + 1) {
+            let pos = (line as f32 - v0.y) * aspect.y;
+            let line_width =
+                if line % 31 != 0 { self.thin_width }
+                else { self.thick_width };
+            let color =
+                if line % 31 != 0 { self.style.base2() }
+                else { self.style.base1() };
+
+            renderer.render_rect(
+                Vector2::new(0.0, pos - 0.5 * line_width),
+                Vector2::new(self.size.x, pos + 0.5 * line_width),
+                color
+            );
+        }
+
+        let beats = self.beats as f32;
+        let (x_first, x_last) = (
+            (v0.x * beats).ceil() as i32,
+            (v1.x * beats).floor() as i32,
+        );
+        for line in x_first..(x_last) + 1 {
+            let pos = (line as f32 / beats - v0.x) * aspect.x;
+            let line_width =
+                if line % self.beats as i32 != 0 { self.thin_width }
+                else { self.thick_width };
+            let color =
+                if line % self.beats as i32 != 0 { self.style.base3() }
+                else { self.style.base2() };
+
+            renderer.render_rect(
+                Vector2::new(pos - 0.5 * line_width, 0.0),
+                Vector2::new(pos + 0.5 * line_width, self.size.y),
+                color
+            )
+        }
+    }
+}
+
+pub struct NoteView {
+    pub notes: Vec<super::Note>,
+    pub view: (Vector2<f32>, Vector2<f32>),
+    pub measure_ticks: u16,
+    pub style: Style,
+}
+
+impl Draw for NoteView {
+    fn draw<R: Render>(&self, size: Vector2<f32>, renderer: &mut R) {
+        use cgmath::ElementWise;
+
+        let aspect = size.div_element_wise(self.view.1 - self.view.0);
+        let brick_width = 1.4 * aspect.y;
+        let color = self.style.orange();
+
+        for note in &self.notes {
+            let start = Vector2::new(
+                note.time.0 as f32 / self.measure_ticks as f32,
+                note.pitch as f32
+            );
+            let end = Vector2::new(
+                note.time.1 as f32 / self.measure_ticks as f32,
+                note.pitch as f32
+            );
+
+            let v0 = (start - self.view.0).mul_element_wise(aspect) - Vector2::new(0.0, brick_width / 2.0);
+            let v1 = (end - self.view.0).mul_element_wise(aspect) + Vector2::new(0.0, brick_width / 2.0);
+
+            //println!("{:?} : {:?}", v0, v1);
+
+            renderer.render_rect(v0, v1, color);
+
+        }
+    }
+}
