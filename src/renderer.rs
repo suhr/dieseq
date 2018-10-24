59f70901de2cc802154f62b3b0cf24eb623e5b59
Сухарик <suhr@i2pmail.org>
2018-10-25 00:13:04 +0300

    Begin the transformation

--- src/renderer.rs
+++ src/renderer.rs
@@ -14,6 +14,70 @@ pub type DepthFormat = gfx::format::DepthStencil;
 
 const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
+pub struct Mesh {
+    vertices: Vec<Vertex>,
+    indices: Vec<u16>,
+}
+
+impl Mesh {
+    pub fn new() -> Self {
+        Mesh {
+            vertices: vec![],
+            indices: vec![],
+        }
+    }
+    pub fn add_rect(&mut self, a0: Vector2<f32>, a1: Vector2<f32>, color: [f32; 4]) {
+        let i0 = self.vertices.len() as u16;
+        let vs = [[a0.x, a0.y], [a0.x, a1.y], [a1.x, a1.y], [a1.x, a0.y]];
+        self.vertices.extend(vs.into_iter().map(|p| Vertex {
+            pos: *p,
+            color: color,
+        }));
+        self.indices.extend(&[i0, i0+1, i0+2, i0+2, i0+3, i0]);
+    }
+
+    pub fn add_fan<V>(&mut self, iter: V)
+    where V: ::std::iter::IntoIterator<Item=Vertex> {
+        let i0 = self.vertices.len() as u16;
+        let mut vs = iter.into_iter();
+        self.vertices.push(vs.next().unwrap());
+        self.vertices.push(vs.next().unwrap());
+        for (i, v) in vs.enumerate() {
+            let i = i as u16 + 1;
+            self.vertices.push(v);
+            self.indices.extend(&[i0, i0+i, i0+i+1]);
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.vertices.clear();
+        self.indices.clear();
+    }
+}
+
+pub enum Object {
+    Mesh(Mesh),
+}
+
+pub struct Scene {
+    objs: Vec<Mesh>,
+}
+
+impl Scene {
+    pub fn new() -> Self {
+        Scene {
+            objs: vec![],
+        }
+    }
+    pub fn add_mesh(&mut self, mesh: Mesh) {
+        self.objs.push(mesh)
+    }
+
+    pub fn clear(&mut self) {
+        self.objs.clear()
+    }
+}
+
 gfx_defines! {
     vertex Vertex {
         pos: [f32; 2] = "a_Pos",
@@ -35,17 +99,11 @@ pub trait Render {
     fn clear(&mut self, color: [f32; 4]);
 }
 
-pub trait Draw {
-    fn draw<R: Render>(&self, screen_size: Vector2<f32>, renderer: &mut R);
-}
-
 pub struct Renderer {
     factory: gl::Factory,
     encoder: Encoder<gl::Resources, gl::CommandBuffer>,
     out_color: RenderTargetView<gl::Resources, ColorFormat>,
     pso: PipelineState<gl::Resources, pipe::Meta>,
-    vertices: Vec<Vertex>,
-    indices: Vec<u16>,
 }
 
 impl Renderer {
@@ -75,6 +133,70 @@ impl Renderer {
 
         Renderer {
             factory, encoder, pso, out_color,
+        }
+    }
+    pub fn render_scene(&mut self, scene: &Scene, screen_size: [f32; 2], device: &mut gl::Device) {
+        for m in scene.objs.iter() {
+            let (vbuf, sl) =
+            self.factory.create_vertex_buffer_with_slice(&m.vertices, &*m.indices);
+
+            let data = pipe::Data {
+                screen: screen_size,
+                vbuf,
+                out: self.out_color.clone(),
+            };
+
+            self.encoder.draw(&sl, &self.pso, &data);
+        }
+
+        self.encoder.flush(device);
+    }
+    pub fn update_views(&mut self, window: &glutin::GlWindow, depth: &mut DepthStencilView<gl::Resources, DepthFormat>) {
+        gfx_glutin::update_views(&window, &mut self.out_color, depth)
+    }
+
+    pub fn clear(&mut self, color: [f32; 4]) {
+        self.encoder.clear(&mut self.out_color, color)
+    }
+}
+
+pub struct OldRenderer {
+    factory: gl::Factory,
+    encoder: Encoder<gl::Resources, gl::CommandBuffer>,
+    out_color: RenderTargetView<gl::Resources, ColorFormat>,
+    pso: PipelineState<gl::Resources, pipe::Meta>,
+    vertices: Vec<Vertex>,
+    indices: Vec<u16>,
+}
+
+
+impl OldRenderer {
+    pub fn new(
+        mut factory: gl::Factory,
+        encoder: Encoder<gl::Resources, gl::CommandBuffer>,
+        out_color: RenderTargetView<gl::Resources, ColorFormat>
+    ) -> Self {
+        use gfx::state::{Rasterizer, MultiSample};
+
+        let vs = factory.create_shader_vertex(
+            include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/plain_150.glslv"))
+        ).unwrap();
+        let ps = factory.create_shader_pixel(
+            include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/plain_150.glslf"))
+        ).unwrap();
+
+        let pso = factory.create_pipeline_state(
+            &gfx::ShaderSet::Simple(vs, ps),
+            gfx::Primitive::TriangleList,
+            Rasterizer {
+                samples: Some(MultiSample),
+                ..Rasterizer::new_fill()
+            },
+            pipe::new(),
+        ).expect("Failed to create a PSO");
+
+        OldRenderer {
+            factory, encoder, pso, out_color,
             vertices: vec![],
             indices: vec![],
         }
@@ -101,7 +223,7 @@ impl Renderer {
     }
 }
 
-impl Render for Renderer {
+impl Render for OldRenderer {
     fn render_fan<V>(&mut self, iter: V)
     where V: ::std::iter::IntoIterator<Item=Vertex> {
         let i0 = self.vertices.len() as u16;
